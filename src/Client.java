16a912ba3b01dd52cbd750453daad6ea793957be
Thomas <thomaspinton@alunos.utfpr.edu.br>
2025-09-24 16:23:17 -0300

    imagem indo e chegando

--- src/Client.java
+++ src/Client.java
@@ -1,11 +1,16 @@
 import javax.annotation.processing.SupportedSourceVersion;
 import java.io.IOException;
 import java.net.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Objects;
 
 public class Client {
     private InetAddress address;
     DatagramSocket socket;
+    int totalPackets;
 
     public Client() throws UnknownHostException, SocketException {
         address = InetAddress.getByName("localhost");
@@ -21,8 +26,20 @@ public class Client {
         System.out.println("Hello, world!");
     }
 
-    public void receive() throws IOException {
+    private int convertByteToInt(byte[] bytes)
+    {
+        int total = 0;
+        for (int i = bytes.length-1; i >= 0 ; i--)
+        {
+           total += (int) bytes[i] * (int) ( Math.pow(127, bytes.length-1-i) );
+        }
+        return total;
+    }
+
+    public ArrayList<byte[]> receive(ArrayList<byte[]> fileContent) throws IOException {
         System.out.println("Receiving");
+        int totalReceived = 0;
+
         while (true)
         {
             byte[] text = new byte[1400];
@@ -31,19 +48,93 @@ public class Client {
 
             socket.receive(packet);
 
-            String received = "";
-            received = new String(packet.getData(), 0, packet.getLength());
+            byte[] received = packet.getData();
+            if (received[0] == (byte) 1)
+                break;
 
-            if (received.length() > 0)
-            {
-                System.out.println("Received: " + received);
+            byte[] packetIDBytes = Arrays.copyOfRange(received, 1, 3);
+            int packetID = convertByteToInt(packetIDBytes);
+            byte[] totalPacketsBytes = Arrays.copyOfRange(received, 3, 5);
+            this.totalPackets = convertByteToInt(totalPacketsBytes);
+
+
+            if (fileContent.size() <= packetID) {
+                // Grow the list with null placeholders
+                while (fileContent.size() <= packetID) {
+                    fileContent.add(null);
+                }
             }
+
+            fileContent.set(packetID, Arrays.copyOfRange(received, 1, received.length));
+
+//            if (received.length() > 0)
+//            {
+                System.out.println("Received id: " + ( packetID ) );
+                totalReceived++;
+//            }
+//            System.out.println("Total Received: " + totalReceived);
+
+            if (packetID == this.totalPackets - 1)
+                break;
+        }
+
+        return fileContent;
+    }
+
+    private void reassembleImage(ArrayList<byte[]> fileContent)
+    {
+        byte[] imageBytes = new byte[fileContent.size() * 1395];
+        for (int i = 0; i < fileContent.size(); i++)
+        {
+            for (int j = 0; j < fileContent.get(i).length-4; j++)
+            {
+                imageBytes[i * 1395 + j] = fileContent.get(i)[j+4];
+            }
+        }
+
+        Path outputPath = Path.of("output.png");
+
+        try {
+            Files.write(outputPath, imageBytes);
+            System.out.println("Image written to " + outputPath);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
     public static void main(String[] args) throws IOException {
         Client client = new Client();
         client.send("GET foto1.png");
-        client.receive();
+        client.totalPackets = 0;
+
+        ArrayList <byte[]> fileContent = new ArrayList<byte[]>();
+
+        int totalPacketsMissing = 1;
+
+        while (totalPacketsMissing > 0)
+        {
+            System.out.println("Packets missing");
+            totalPacketsMissing = 0;
+            fileContent = client.receive(fileContent);
+
+            // verify packets
+            String packetsMissing = "";
+            System.out.println("Size: " + fileContent.size());
+            for (int i = 0; i < client.totalPackets && totalPacketsMissing < 10; i++)
+            {
+                if (fileContent.get(i) == null)
+                {
+                    packetsMissing += i + " ";
+                    totalPacketsMissing++;
+                    System.out.println("Packets Missing: " + i);
+                }
+            }
+            client.send("NAC " + packetsMissing);
+            System.out.println("Size: " + fileContent.size());
+        }
+
+        client.reassembleImage(fileContent);
+
+
     }
 }
--- src/Server.java
+++ src/Server.java
@@ -7,6 +7,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.*;
 import java.nio.file.Files;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Objects;
 
@@ -14,6 +15,7 @@ public class Server
 {
     DatagramSocket socket;
     private InetAddress address;
+    ArrayList<byte[]> packages = new ArrayList<byte[]>();
 
     Server() throws SocketException, UnknownHostException {
         socket = new DatagramSocket(1234); // Listens on 1234
@@ -21,21 +23,26 @@ public class Server
     }
 
     public void receive() throws IOException {
-        byte[] text = new byte[256];
-
-        DatagramPacket packet = new DatagramPacket(text, text.length);
-
-        socket.receive(packet);
-
-        String received = "";
-        received = new String(packet.getData(), 0, packet.getLength());
-
-        if (received.length() > 0)
+        while (true)
         {
-            System.out.println("Received: " + received);
-            String[] parts = received.split(" ");
-            if (Objects.equals(parts[0], "GET"))
-                get(parts[1]);
+            byte[] text = new byte[256];
+
+            DatagramPacket packet = new DatagramPacket(text, text.length);
+
+            socket.receive(packet);
+
+            String received = "";
+            received = new String(packet.getData(), 0, packet.getLength());
+
+            if (received.length() > 0) {
+                System.out.println("Received: " + received);
+                String[] parts = received.split(" ", 2);
+                if (Objects.equals(parts[0], "GET"))
+                    get(parts[1]);
+                else if (Objects.equals(parts[0], "NAC")) {
+                    nack(parts[1]);
+                }
+            }
         }
     }
 
@@ -43,6 +50,19 @@ public class Server
     {
         System.out.println("Getting " + text);
         splitImage(text);
+    }
+
+    private void nack(String text)
+    {
+        String[] missingPackets = text.split(" ", -1);
+        for (String missingPacket : missingPackets) {
+            if (!Objects.equals(missingPacket, "")) {
+                send(packages.get(Integer.parseInt(missingPacket)));
+                System.out.println("Packets: " +Integer.parseInt(missingPacket));
+            } else {
+                sendEndMessage();
+            }
+        }
     }
 
     private void splitImage(String name)
@@ -56,40 +76,69 @@ public class Server
         }
         int length = fileContent.length;
         System.out.println("Length: " + length);
-        int datagramSize = 1400;
+        int datagramSize = 1395;
         int i = 0, j = datagramSize;
-        int steps = (int) Math.ceil((double) length / 1400);
+        int steps = (int) Math.ceil((double) length / datagramSize);
 
-        byte[] dataToSend = null;
+        byte[] id = new byte[2];
 
-        for (int k = 0; k < 1; k++)
+        for (int k = 0; k < steps; k++)
         {
-            dataToSend = Arrays.copyOfRange(fileContent, i, j);
+            byte[] dataToSend = new byte[1400];
+            byte[] data = Arrays.copyOfRange(fileContent, i, j);
 
+            for (int l = 5; l < data.length + 5; l++)
+                dataToSend[l] = data[l-5];
+
+            // Header pt0: type of message
+            dataToSend[0] = (byte) 0;
+                // 0 = sending data
+                // 1 = message end
+
+            // Header pt1: packet ID
+            dataToSend[1] = id[0];
+            dataToSend[2] = id[1];
+
+            // Header pt2: totalPackets
+            dataToSend[3] = (byte) ( steps / 127 );
+            dataToSend[4] = (byte) ( steps % 127 );
+
+            packages.add(dataToSend);
             send(dataToSend);
 
+            // update Values
             i = j;
             j += datagramSize;
             if (j > fileContent.length)
                 j = fileContent.length;
+
+            // update Id
+            id[1]++;
+            if (id[1] == 127)
+            {
+                id[1] = 0;
+                id[0] ++;
+            }
+
         }
+    }
+
+    private void sendEndMessage()
+    {
+        byte[] dataToSend = new byte[1400];
+        dataToSend[0] = (byte) 1;
+        // 1 = message end
+        send(dataToSend);
     }
 
     private void send(byte[] dataToSend)
     {
-//        int i = 0;
-//        File file = new File("../media/split/img" + i + ".jpg");
-//        byte[] fileData = new byte[(int) file.length()];
-//        try (FileInputStream fis = new FileInputStream(file)) {
-//            fis.read(fileData);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
         DatagramPacket packet = new DatagramPacket(dataToSend, dataToSend.length, address, 1235);
+
+        System.out.println("Sending sent: id " + ( dataToSend[1] * 127 + dataToSend[2] ));
 
         try {
             socket.send(packet);
-//            System.out.println("Packet sent: id ");
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
